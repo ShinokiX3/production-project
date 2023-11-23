813895f0252ed8e230c5c490e0aae597838b68b1
shinoki <endernasha85@gmail.com>
2023-11-23 19:57:48 +0300

    initial App settings

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { useContext } from 'react';
+import { Route, Routes, Link } from 'react-router-dom';
+import './styles/index.scss';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import { Suspense, useState } from 'react';
+import { useTheme } from './theme/useTheme';
+
+const App = () => {
+	const { theme, toggleTheme } = useTheme();
+
+	return (
+		<div className={`app ${theme}`}>
+			<button onClick={toggleTheme}>Toggle theme</button>
+			<Link to={'/'}>Main</Link>
+			<Link to={'/about'}>About</Link>
+			<Suspense fallback={<div>Loading...</div>}>
+				<Routes>
+					<Route path={'about'} element={<AboutPageAsync />} />
+					<Route path={'/'} element={<MainPageAsync />} />
+				</Routes>
+			</Suspense>
+			Text
+		</div>
+	);
+};
+
+export default App;
