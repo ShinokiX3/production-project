60995cf0f714aa808f8120be38d4aeed7eecc11f
shinoki <endernasha85@gmail.com>
2023-12-02 16:56:45 +0300

    redux rtk setup

--- src/shared/lib/tests/componentRender/componentRender.tsx
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -1,22 +1,28 @@
 import { render } from '@testing-library/react';
+import { StateSchema, StoreProvider } from 'app/providers/StoreProvider';
 import { I18nextProvider } from 'react-i18next';
 import { MemoryRouter } from 'react-router-dom';
+import { DeepPartial } from '@reduxjs/toolkit';
 import i18nForTests from 'shared/config/i18n/i18nForTests';
 
 export interface ComponentRenderOptions {
-    route?: string
+    route?: string,
+	initialState?: DeepPartial<StateSchema>
 }
 
 export function componentRender(component: React.ReactNode, options: ComponentRenderOptions = {}) {
 	const {
 		route = '/',
+		initialState,
 	} = options;
 
 	return render(
-		<MemoryRouter initialEntries={[route]}>
-			<I18nextProvider i18n={i18nForTests}>
-				{component}
-			</I18nextProvider>
-		</MemoryRouter>
+		<StoreProvider initialState={initialState}>
+			<MemoryRouter initialEntries={[route]}>
+				<I18nextProvider i18n={i18nForTests}>
+					{component}
+				</I18nextProvider>
+			</MemoryRouter>
+		</StoreProvider>
 	);
 }
