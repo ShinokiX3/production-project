0781bac065976921de3fa440cb4663948872b656
shinoki <endernasha85@gmail.com>
2024-05-18 01:28:03 +0300

    first production

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.async.ts
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.async.ts
@@ -1,6 +1,4 @@
 import { lazy } from 'react';
 import { LoginFormProps } from './LoginForm';
 
-export const LoginFormAsync = lazy<React.FC<LoginFormProps>>(() => new Promise((resolve) => {
-	setTimeout(() => resolve(import('./LoginForm')), 1500);
-}));
+export const LoginFormAsync = lazy<React.FC<LoginFormProps>>(() => import('./LoginForm'));
--- src/features/addCommentForm/ui/AddCommentForm/AddCommentForm.async.tsx
+++ src/features/addCommentForm/ui/AddCommentForm/AddCommentForm.async.tsx
@@ -1,8 +1,4 @@
 import { lazy } from 'react';
 import { AddCommentFormProps } from './AddCommentForm';
 
-export const AddCommentFormAsync = lazy<React.FC<AddCommentFormProps>>(() => new Promise((resolve) => {
-	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-	// @ts-ignore
-	setTimeout(() => resolve(import('./AddCommentForm')), 1500);
-}));
+export const AddCommentFormAsync = lazy<React.FC<AddCommentFormProps>>(() => import('./AddCommentForm'));
