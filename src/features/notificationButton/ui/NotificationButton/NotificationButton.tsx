071bb408698ae557b4ea6ca6279da82d4cc39a54
shinoki <endernasha85@gmail.com>
2024-06-05 03:58:21 +0300

    add: * gestures, * react spring, * dynamic libs loading with custom provider

--- src/features/notificationButton/ui/NotificationButton/NotificationButton.tsx
+++ src/features/notificationButton/ui/NotificationButton/NotificationButton.tsx
@@ -8,6 +8,7 @@ import NotificationIcon from 'shared/assets/icons/notification-20-20.svg';
 import { NotificationList } from 'entities/Notification';
 import { Drawer } from 'shared/ui/Drawer/Drawer';
 import { BrowserView, MobileView } from 'react-device-detect';
+import { AnimationProvider } from 'shared/lib/components/AnimationProvider';
 import cls from './NotificationButton.module.scss';
 
 interface NotificationButtonProps {
@@ -40,12 +41,13 @@ export const NotificationButton = memo((props: NotificationButtonProps) => {
 					<NotificationList className={cls.notifications} />
 				</Popover>
 			</BrowserView>
-
 			<MobileView>
-				{trigger}
-				<Drawer isOpen={isDrawer} onClose={closeDrawer}>
-					<NotificationList />
-				</Drawer>
+				<AnimationProvider>
+					{trigger}
+					<Drawer isOpen={isDrawer} onClose={closeDrawer}>
+						<NotificationList />
+					</Drawer>
+				</AnimationProvider>
 			</MobileView>
 		</div>
 	);
