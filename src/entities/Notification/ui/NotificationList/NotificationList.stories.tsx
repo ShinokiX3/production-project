b543326fdbfe139e6736b359a794cf749f366492
shinoki <endernasha85@gmail.com>
2024-06-06 15:19:42 +0300

    add: * storybook's request mock for notifications list & article rating. fix: * storybook's alias & paths

--- src/entities/Notification/ui/NotificationList/NotificationList.stories.tsx
+++ src/entities/Notification/ui/NotificationList/NotificationList.stories.tsx
@@ -1,15 +1,35 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
+import withMock from 'storybook-addon-mock';
 import { NotificationList } from './NotificationList';
+import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator';
 
 export default {
-	title: 'dir/NotificationList',
+	title: 'entities/Notification/NotificationList',
 	component: NotificationList,
 	argTypes: {
 		backgroundColor: { control: 'color' },
 	},
+	decorators: [withMock]
 } as ComponentMeta<typeof NotificationList>;
 
 const Template: ComponentStory<typeof NotificationList> = (args) => <NotificationList {...args} />;
 
 export const Normal = Template.bind({});
 Normal.args = {};
+Normal.decorators = [StoreDecorator({})];
+Normal.parameters = {
+	mockData: [
+		{
+			url: `${__API__}/notifications`,
+			method: 'GET',
+			status: 200,
+			response: [
+				{
+					id: '1',
+					title: 'Message',
+					description: 'Description',
+				}
+			]
+		}
+	]
+};
