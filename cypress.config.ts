25fb824a511c96e704832bd85f8a1f4ea506000f
shinoki <endernasha85@gmail.com>
2024-06-12 16:37:33 +0300

    add: * tests for isolated components with cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -7,5 +7,13 @@ export default defineConfig({
 		},
 		baseUrl: 'http://localhost:5173' || 'http://localhost:3000',
 	},
-	port: 4000
+
+	port: 4000,
+
+	component: {
+		devServer: {
+			framework: 'react',
+			bundler: 'webpack'
+		}
+	}
 });
--- src/shared/lib/tests/componentRender/componentRender.tsx
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -4,27 +4,52 @@ import { I18nextProvider } from 'react-i18next';
 import { MemoryRouter } from 'react-router-dom';
 import { StateSchema, StoreProvider } from '@/app/providers/StoreProvider';
 import i18nForTests from '@/shared/config/i18n/i18nForTests';
+import { Theme, ThemeProvider } from '@/app/providers/ThemeProvider';
+import '@/app/styles/index.scss';
 
 export interface ComponentRenderOptions {
     route?: string,
 	initialState?: DeepPartial<StateSchema>,
 	asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>
+	theme?: Theme;
 }
 
-export function componentRender(component: React.ReactNode, options: ComponentRenderOptions = {}) {
+interface TestProviderProps {
+	children: React.ReactNode;
+	options?: ComponentRenderOptions;
+}
+
+export function TestProvider(props: TestProviderProps) {
+	const {
+		children,
+		options = {}
+	} = props;
 	const {
 		route = '/',
 		initialState,
-		asyncReducers
+		asyncReducers,
+		theme = Theme.LIGHT
 	} = options;
 
-	return render(
+	return (
 		<MemoryRouter initialEntries={[route]}>
 			<StoreProvider asyncReducers={asyncReducers} initialState={initialState}>
 				<I18nextProvider i18n={i18nForTests}>
-					{component}
+					<ThemeProvider initialTheme={theme}>
+						<div className={`app ${theme}`}>
+							{children}
+						</div>
+					</ThemeProvider>
 				</I18nextProvider>
 			</StoreProvider>
-		</MemoryRouter>,
+		</MemoryRouter>
+	);
+}
+
+export function componentRender(component: React.ReactNode, options: ComponentRenderOptions = {}) {
+	return render(
+		<TestProvider options={options}>
+			{ component }
+		</TestProvider>
 	);
 }
