9b44ab008fe2d88c9a3bf2b3550cc1ffb7287de8
shinoki <endernasha85@gmail.com>
2024-06-14 20:48:19 +0300

    get api url function

--- webpack.config.ts
+++ webpack.config.ts
@@ -3,6 +3,17 @@ import webpack from 'webpack';
 import { buildWebpackConfig } from './config/build/buildWebpackConfig';
 import { BuildEnv, BuildPaths } from './config/build/types/config';
 
+const getApiUrl = (mode: BuildEnv['mode']) => {
+    if (mode === 'production') {
+        return 'https://production-project-server-nu-peach.vercel.app';
+    }
+    if (mode === 'development') {
+        return 'http://localhost:8000';
+    }
+
+    return 'http://localhost:8000';
+};
+
 export default (env: BuildEnv) => {
     const paths: BuildPaths = {
         entry: path.resolve(__dirname, 'src', 'index.tsx'),
@@ -16,7 +27,7 @@ export default (env: BuildEnv) => {
     const mode = env?.mode || 'development';
     const isDev = mode === 'development';
     const PORT = env?.port || 3000;
-    const apiUrl = env?.apiUrl || 'http://localhost:8000';
+    const apiUrl = env?.apiUrl || getApiUrl(mode);
 
     const config: webpack.Configuration = buildWebpackConfig({
         mode,
