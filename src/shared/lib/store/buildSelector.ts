fb215de91359eb89190249e1927f9b2879d4accd
shinoki <endernasha85@gmail.com>
2024-06-15 20:30:45 +0300

    add: * arguments for selector hook

--- src/shared/lib/store/buildSelector.ts
+++ src/shared/lib/store/buildSelector.ts
@@ -1,11 +1,16 @@
 import { useSelector } from 'react-redux';
 import { StateSchema } from '@/app/providers/StoreProvider';
 
-export type Selector<T> = (state: StateSchema) => T;
-export type Result<T> = [() => T, Selector<T>];
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type Args = any[];
 
-export function buildSelector<T>(selector: Selector<T>): Result<T> {
-    const useSelctorHook = () => useSelector(selector);
+export type Selector<T, A extends Args> = (state: StateSchema, ...args: A) => T;
+export type Hook<T, A extends Args> = (...args: A) => T;
+export type Result<T, A extends Args> = [Hook<T, A>, Selector<T, A>];
+
+export function buildSelector<T, A extends Args>(selector: Selector<T, A>): Result<T, A> {
+    const useSelctorHook: Hook<T, A> = (...args: A) => 
+        useSelector((state: StateSchema) => selector(state, ...args));
 
     return [useSelctorHook, selector];
 }
