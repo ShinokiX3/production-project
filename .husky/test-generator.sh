5430998456428b63f13da1ecd2574865f660e4e8
ShinokiX3 <endernasha85@gmail.com>
2025-05-29 23:10:19 +0300

    test commit

--- .husky/test-generator.sh
+++ .husky/test-generator.sh
@@ -34,8 +34,9 @@ check_branch() {
     local current_branch=$(git branch --show-current)
     log_info "Текущая ветка: $current_branch"
     
-    if [[ ! $current_branch =~ ^FEDS- ]]; then
-        log_warn "Ветка не начинается с 'FEDS-', пропускаем проверки"
+    # if [[ ! $current_branch =~ ^FEDS- ]]; then
+    if [[ ! $current_branch =~ ^main ]]; then
+        log_warn "Ветка не начинается с 'main', пропускаем проверки"
         exit 0
     fi
     
